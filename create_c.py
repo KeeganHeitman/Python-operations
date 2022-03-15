19b12c81e63bbc09e109b2eaa36a1528e3bd4a4a
KeeganHeitman <74746845+KeeganHeitman@users.noreply.github.com>
2022-03-14 17:39:10 -0700

    Update create_c.py

--- create_c.py
+++ create_c.py
@@ -4,3 +4,10 @@ def create(f,n):
   subprocess.run(rm(f))
   pass
 create(input(),input())
+
+#raises error "Traceback (most recent call last):
+  File "/home/ghaspingghasp/create.py", line 6, in <module>
+    create(input(),input())
+  File "/home/ghaspingghasp/create.py", line 3, in create
+    subprocess.run(gcc -o (newname) (filename))
+NameError: name 'gcc' is not defined"
